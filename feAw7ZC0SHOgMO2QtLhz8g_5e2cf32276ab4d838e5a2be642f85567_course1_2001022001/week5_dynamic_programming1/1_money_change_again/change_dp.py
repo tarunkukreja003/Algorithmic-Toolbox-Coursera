20fb6c99f8a08e304ef6a1fb19c178c0f16a4540
Tarun Kukreja <tarunkukreja@Taruns-MacBook-Pro.local>
2020-04-07 15:32:43 +0530

    money change with dynamic programming added

--- feAw7ZC0SHOgMO2QtLhz8g_5e2cf32276ab4d838e5a2be642f85567_course1_2001022001/week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ feAw7ZC0SHOgMO2QtLhz8g_5e2cf32276ab4d838e5a2be642f85567_course1_2001022001/week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -1,10 +1,27 @@
 # Uses python3
-import sys
 
-def get_change(m):
-    #write your code here
-    return m // 4
+########## Money Change using Dynamic Programming
+  
+def dp_change(money):
+    coin_sorted_array = [1, 3, 4]
+    min_no_of_coins = []
+    if money <= 0:
+        return 0
+    min_no_of_coins.append(0)
+    
+    for m in range(1, money+1):
+        min_no_of_coins.append(10000) # min_no_of_coins[m] = 10000
+        
+        for i in range(len(coin_sorted_array)):
+            if m >= coin_sorted_array[i]:
+                num_of_coins = min_no_of_coins[m - coin_sorted_array[i]] + 1
+            if num_of_coins < min_no_of_coins[m]:
+                min_no_of_coins[m] = num_of_coins
+    return min_no_of_coins[money]
+            
+
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
-    print(get_change(m))
+    n = int(input())
+    min_number_coins_n = dp_change(n)
+    print(min_number_coins_n)
