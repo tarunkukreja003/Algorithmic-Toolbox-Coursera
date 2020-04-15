6f84aa02902a775d690fbfe9c497d18c057accbd
Tarun Kukreja <tarunkukreja@Taruns-MacBook-Pro.local>
2020-04-15 19:14:41 +0530

    knapsack and paranthesis code added

--- feAw7ZC0SHOgMO2QtLhz8g_5e2cf32276ab4d838e5a2be642f85567_course1_2001022001/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ feAw7ZC0SHOgMO2QtLhz8g_5e2cf32276ab4d838e5a2be642f85567_course1_2001022001/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -1,15 +1,31 @@
 # Uses python3
-import sys
 
-def optimal_weight(W, w):
-    # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+def max_gold(W, n, arr_w):
+    ## assigning value to each gold bar. 1kg gold bar has value $1
+    ## value is same as weight since more the gold more the value
+#     arr_v = arr_w
+    
+    value_table = [[0 for j in range(W + 1)] for i in range(n + 1)]
+    
+    for i in range(1, n + 1):
+        
+        for w in range(1, W + 1):
+            
+            value_table[i][w] = value_table[i-1][w]
+            
+            if arr_w[i-1] <= w:
+            
+                val = value_table[i-1][w - arr_w[i-1]] + arr_w[i-1]
+                
+                if val > value_table[i][w]:
+                    value_table[i][w] = val
+            
+    
+    
+    return value_table[n][W]
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    W, n, *w = list(map(int, input.split()))
-    print(optimal_weight(W, w))
+    W, n = map(int, input().split())
+    arr_w = list(map(int, input().split()))
+    max_val = max_gold(W, n, arr_w)
+    print(max_val)
--- feAw7ZC0SHOgMO2QtLhz8g_5e2cf32276ab4d838e5a2be642f85567_course1_2001022001/week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
+++ feAw7ZC0SHOgMO2QtLhz8g_5e2cf32276ab4d838e5a2be642f85567_course1_2001022001/week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
@@ -1,18 +1,65 @@
 # Uses python3
-def evalt(a, b, op):
-    if op == '+':
-        return a + b
-    elif op == '-':
-        return a - b
-    elif op == '*':
-        return a * b
+def calc(operand_1, operand_2, operator):
+    if operator == '+':
+        return (operand_1 + operand_2)
+    elif operator == '-':
+        return (operand_1 - operand_2)
     else:
-        assert False
+        return (operand_1 * operand_2)
 
-def get_maximum_value(dataset):
-    #write your code here
-    return 0
+def get_max_min(table_max, table_min, i, j, operator_arr):
+    min_i_j = 1000000
+    max_i_j = -1000000
+    
+#     print('insie get_max_min i = {}, j = {} '.format(i, j))
+#     print('table_max after getting inside get_max_min ', table_max)
+    
+    for k in range(i, j):
+        a = calc(table_max[i][k], table_max[k+1][j], operator_arr[k])
+        b = calc(table_max[i][k], table_min[k+1][j], operator_arr[k])
+        c = calc(table_min[i][k], table_max[k+1][j], operator_arr[k])
+        d = calc(table_min[i][k], table_min[k+1][j], operator_arr[k])
+        
+        
+        if max_i_j < max(a, b, c, d):
+            max_i_j = max(a, b, c, d)
+        if min_i_j > min(a, b, c, d):
+            min_i_j = min(a, b, c, d)
+    
+    return (max_i_j, min_i_j)
+    
+        
+
+def paranthesis(operator_arr, ele_arr, n):
+    table_max = [[0 for j in range(0, n)] for i in range(0, n)]
+    table_min = [[0 for j in range(0, n)] for i in range(0, n)]
+    
+    for i in range(0, n):
+        for j in range(0, n):
+            if i == j:
+                table_max[i][j] = int(ele_arr[i])
+                table_min[i][j] = int(ele_arr[i])
+                
+    ### its upper triangle of the matrix where column >= row 
+    ## s is just the difference between j and i
+    for s in range(1, n):
+        for i in range(0, n - s):
+            j = i + s
+            table_max[i][j], table_min[i][j] = get_max_min(table_max, table_min, i, j, operator_arr)
+            
+            
+    return table_max[0][n-1]
+
 
 
 if __name__ == "__main__":
-    print(get_maximum_value(input()))
+    expression_str = str(input())
+    operand_array, operator_array = [], []
+    for i in range(len(expression_str)):
+    
+        if i % 2 == 0:
+            operand_array.append(expression_str[i])
+        else:
+            operator_array.append(expression_str[i])
+    max_value = paranthesis(operator_array, operand_array, len(operand_array))
+    print(max_value)
